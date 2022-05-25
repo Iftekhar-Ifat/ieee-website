db7f39b3070c27c909118c7f37adbb4489c5fe4c
Iftekhar Ahmed <iftekharifat007@gmail.com>
2022-05-25 18:07:06 +0600

    made swiper responsive

--- src/components/HomePageComponents/SwiperJS.js
+++ src/components/HomePageComponents/SwiperJS.js
@@ -1,12 +1,12 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import user from '../../styles/components/Images/user.png'
+import user from "../../styles/components/Images/user.png";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 
-import styles from "../../styles/components/HomePageStyles/SwiperJS.module.css"
+import styles from "../../styles/components/HomePageStyles/SwiperJS.module.css";
 
 // import required modules
 import { Navigation } from "swiper";
@@ -14,31 +14,52 @@ import { Navigation } from "swiper";
 const SwiperJS = () => {
     return (
         <>
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+            <Swiper
+                navigation={true}
+                modules={[Navigation]}
+                className="mySwiper"
+            >
                 {Array.from({ length: 3 }).map((_, idx) => (
                     <SwiperSlide>
-
                         <div className={styles.review_card}>
                             {/* photo */}
                             <div className={styles.user_img_container}>
-                                <img className={styles.user_img} src={user} alt="" height={90} width90 />
+                                <img
+                                    className={styles.user_img}
+                                    src={user}
+                                    alt=""
+                                    height={90}
+                                />
                             </div>
 
                             {/* name & title  */}
                             <div className={styles.user_info}>
-                                <p className="text-center"><span className="text-primary fw-bold">User Name</span>  <br /> <span className="text-muted">User Title</span>  </p>
+                                <p className="text-center">
+                                    <span className="text-primary fw-bold">
+                                        User Name
+                                    </span>{" "}
+                                    <br />{" "}
+                                    <span className="text-muted">
+                                        User Title
+                                    </span>{" "}
+                                </p>
                             </div>
 
                             {/* testimonial */}
                             <div>
-                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil, commodi aut ipsa placeat libero quaerat sed, exercitationem facere repellat labore atque quod optio consectetur sint aperiam natus quibusdam ratione debitis.
+                                <p>
+                                    Lorem ipsum dolor sit amet consectetur,
+                                    adipisicing elit. Nihil, commodi aut ipsa
+                                    placeat libero quaerat sed, exercitationem
+                                    facere repellat labore atque quod optio
+                                    consectetur sint aperiam natus quibusdam
+                                    ratione debitis.
                                 </p>
                             </div>
                         </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
-
         </>
     );
 };
